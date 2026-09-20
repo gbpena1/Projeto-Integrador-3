7ed33eaea023dc51d99daddc262d55c4e77ce49f
Daniel Jonas <daniel.governador@gmail.com>
2026-09-20 02:19:14 -0300

    adicionando componentes de cartas, checkout e detalhes do produto no mvp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { ProductDetail } from './pages/product-detail/product-detail';
+import { Cart } from './pages/cart/cart';
+import { Checkout } from './pages/checkout/checkout';     
 
 export const routes: Routes = [
     {
@@ -10,5 +12,13 @@ export const routes: Routes = [
     {
         path: 'product',
         component: ProductDetail
+    },
+    {
+        path: 'cart',
+        component: Cart
+    } ,
+    {
+        path: 'checkout',
+        component: Checkout
     }
 ];
